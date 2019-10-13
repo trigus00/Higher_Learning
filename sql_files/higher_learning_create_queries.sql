a94784f7487cd100e44d3f28dbff2bc89bcc0197
Bivittatus <Python.Bivittatus.8518@gmail.com>
2019-10-13 18:39:41 -0500

    10/13/2019 completed python data cleaning and postgresql data push

--- sql_files/higher_learning_create_queries.sql
+++ sql_files/higher_learning_create_queries.sql
@@ -1,33 +1,67 @@
 DROP TABLE IF EXISTS incarceration;
-DROP TABLE IF EXISTS fourth_grade_read;
+DROP TABLE IF EXISTS ed_enroll_exp;
 DROP TABLE IF EXISTS prisoner_exp;
-DROP TABLE IF EXISTS education_enroll_exp;
+DROP TABLE IF EXISTS fourth_grade_read;
+DROP TABLE IF EXISTS ed_corr_data;
 
 
 CREATE TABLE incarceration (
-  yr_state PRIMARY KEY,
-  state character varying(250),
-  year character varying(350),
+  state character varying(50),
   year integer,
-  citation_journal_title character varying(100),
-  description character varying(3000),
-  keywords character varying(1000)
+  prisoner_count integer,
+  state_population integer,
+  year_state character varying(100) PRIMARY KEY
+);
+
+
+CREATE TABLE ed_enroll_exp (
+  year_state character varying(100) PRIMARY KEY,
+  state character varying(50),
+  year integer,
+  enrollment integer,
+  total_expenditure integer
+);
+
+
+CREATE TABLE prisoner_exp (
+  state character varying(50),
+  year integer,
+  total_expenditure integer,
+  year_state character varying(100) PRIMARY KEY
+);
+
+
+CREATE TABLE fourth_grade_read (
+  state character varying(50),
+  achievement_level character varying(50),
+  year integer,
+  data integer,
+  year_state_ach_lvl character varying(100) PRIMARY KEY
 );
 
 
 
 
-
-
-
-
-
-
-
+CREATE TABLE ed_corr_data (
+year_state character varying(100) PRIMARY KEY,
+state character varying(50),
+ed_enrollment integer,
+ed_total_exp integer,
+prisoner_count integer,
+corr_total_exp integer,
+state_population integer,
+cost_per_stu float,
+stu_to_pop_percent float,
+cost_per_prisoner float,
+prisoner_to_pop_percent float
+);
 
 
 SELECT * FROM incarceration;
-SELECT * FROM fourth_grade_read;
+SELECT * FROM ed_enroll_exp;
 SELECT * FROM prisoner_exp;
-SELECT * FROM education_enroll_exp;
+SELECT * FROM fourth_grade_read;
+SELECT * FROM ed_corr_data;
+
+
 
